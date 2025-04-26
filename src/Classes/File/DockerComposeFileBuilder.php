ab18af5b5bcd63831aac458dd288e39d88350c0c
Danny <daniel@winningsoftware.co.uk>
2025-04-26 14:47:59 +0100

    Fix for SQLite build

--- src/Classes/File/DockerComposeFileBuilder.php
+++ src/Classes/File/DockerComposeFileBuilder.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Loom\Spinner\Classes\File;
 
 use Loom\Spinner\Classes\Config\Config;
-use Loom\Utility\FilePath\FilePath;
 use Symfony\Component\Console\Input\InputInterface;
 
 class DockerComposeFileBuilder extends AbstractFileBuilder
